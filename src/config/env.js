71d4f78a6580aeec42332616c080ad2392a8fe2f
hassan mehdi <hassansahsfa64@gmail.com>
2026-03-08 14:54:48 +0500

    backend

--- src/config/env.js
+++ src/config/env.js
@@ -4,7 +4,11 @@ dotenv.config();
 
 export const env = {
   port: Number(process.env.PORT || 5000),
-  mongoUri: process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/north-luxe",
+  mongoUri:
+    process.env.MONGODB_URI ||
+    process.env.MONGO_URI ||
+    "mongodb://127.0.0.1:27017/north-luxe",
+
   clientOrigin: process.env.CLIENT_ORIGIN || "http://localhost:5173",
   jwtSecret: process.env.JWT_SECRET || "change-me",
   jwtExpiresIn: process.env.JWT_EXPIRES_IN || "7d",
