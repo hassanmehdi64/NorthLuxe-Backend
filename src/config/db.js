fe9904b5785fee3bffd74690d187ba4715b7274a
hassan mehdi <hassansahsfa64@gmail.com>
2026-03-08 15:35:51 +0500

    path changes

--- src/config/db.js
+++ src/config/db.js
@@ -1,7 +1,30 @@
 import mongoose from "mongoose";
 import { env } from "./env.js";
 
+let connectionPromise = null;
+
 export const connectDb = async () => {
+  if (!env.mongoUri) {
+    throw new Error("MONGODB_URI is not configured");
+  }
+
   mongoose.set("strictQuery", true);
-  await mongoose.connect(env.mongoUri);
+
+  if (mongoose.connection.readyState === 1) {
+    return mongoose.connection;
+  }
+
+  if (!connectionPromise) {
+    connectionPromise = mongoose.connect(env.mongoUri, {
+      serverSelectionTimeoutMS: 10000,
+    });
+  }
+
+  try {
+    await connectionPromise;
+    return mongoose.connection;
+  } catch (error) {
+    connectionPromise = null;
+    throw error;
+  }
 };
